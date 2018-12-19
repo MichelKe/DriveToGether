7fc3dd1b460915adeb6ff12f35fb49cbd969b891
RemoBaerlocher <45824902+RemoBaerlocher@users.noreply.github.com>
2018-12-19 13:09:45 +0100

    added models

---
+++ DriveToGether/Models/CarModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DriveToGether.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Fahrer { get; set; }
+        public string Details { get; set; }
+        public int Plaetze { get; set; }
+    }
+
+    public class CarDBContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
